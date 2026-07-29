5151a25dd5c85aa88458e1d9092058e58ce67e88
Kyle Robertson <kyle59287@tutanota.com>
2026-07-29 12:02:09 -0700

    Updated design

--- scripts/universal.js
+++ scripts/universal.js
@@ -5,17 +5,41 @@ class NavHeader extends HTMLElement {
         const shadow = this.attachShadow({mode: "open"})
         shadow.innerHTML = `
             <link rel="stylesheet" href="/styles/universal.css">
+            <style>
+                #nav_header {
+                    position: relative; 
+                }
+                #nh_content {
+                    position: absolute;
+                    top: 0;
+                    left: 0;
+                    width: 100%; 
+                }
+                
+                #nh_background {
+                    width: 100%;
+                    height: 100%;
+                }
+            </style>
 
             <div id="nav_header">
-                <h1>
-                    Kyle's Website
-                </h1>
-        
-                <a href="/pages/home.html"><h4>Home</h4></a>
-                <a href="/pages/about_me.html"><h4>About Me</h4></a>
-                <a href="/pages/photography.html"><h4>Photography</h4></a>
+                <img id="nh_background">
+            
+                <div id="nh_content">
+                    <h1>
+                        Kyle's Website
+                    </h1>
+            
+                    <a href="/pages/home.html"><h4>Home</h4></a>
+                    <a href="/pages/about_me.html"><h4>About Me</h4></a>
+                    <a href="/pages/photography.html"><h4>Photography</h4></a>
+                </div>
             </div>
         `
+    }
+
+    setBackground(img) {
+        this.shadowRoot.getElementById("nh_background").setAttribute("src", img)
     }
 }
 customElements.define("nav-header", NavHeader)
